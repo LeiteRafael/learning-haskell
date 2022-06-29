e92703318ef159f06589e5ee79ebdb3d45d251a8
Rafael Leite <rafanleite@gmail.com>
2022-06-29 10:32:49 -0300

    added exercise 4 and 5

--- CATP.hs
+++ CATP.hs
@@ -10,3 +10,9 @@ ocorrencias [] _ = 0 ; ocorrencias (h:t) n | h == n = 1 + (ocorrencias t n) | ot
 somatorio lista = foldr (+) 0 lista
 
 -- 4)
+
+replica e n | n == 0 = [] | otherwise = [e] ++ (replica e (n-1))
+
+-- 5)
+
+norma lista = sqrt (foldr (+) 0 ((map (\x -> x ^ 2)) lista))
