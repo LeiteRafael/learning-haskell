1bbdca3a2fcf5309fd28a3839494431150ffb8c2
Rafael Leite <rafanleite@gmail.com>
2022-06-29 20:03:27 -0300

    implemented 6,7,8 and 9

--- CATP.hs
+++ CATP.hs
@@ -16,3 +16,22 @@ replica e n | n == 0 = [] | otherwise = [e] ++ (replica e (n-1))
 -- 5)
 
 norma lista = sqrt (foldr (+) 0 ((map (\x -> x ^ 2)) lista))
+
+-- 6)
+
+prod_int lista1 lista2 = foldr (+) 0 (zipWith (*) lista1 lista2)
+
+-- 7)
+
+let { norma lista = sqrt (foldr (+) 0 ((map (\x -> x ^ 2)) lista)) }
+let { prod_int lista1 lista2 = foldr (+) 0 (zipWith (*) lista1 lista2) }
+sim a b = (prod_int a b) / ((norma a) * (norma b))
+
+-- 8)
+
+retornaPot2 tamanho = [2^tamanho | tamanho <- [1..tamanho]]
+
+-- 9)
+
+let { fib 0 = 0 ; fib 1 = 1 ; fib n = fib (n-1) + fib (n-2) }
+retornaFib tamanho = [fib tamanho | tamanho <- [1..tamanho]]
